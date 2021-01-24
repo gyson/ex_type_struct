f1bb9082ec1f85faa6bf7c00186fab0a63fbc972
gyson <eilian.yunsong@gmail.com>
2021-01-24 17:59:15 -0500

    Fix ci

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ExTypeStruct.MixProject do
     [
       app: :ex_type_struct,
       version: "0.1.0",
-      elixir: "~> 1.11",
+      elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
